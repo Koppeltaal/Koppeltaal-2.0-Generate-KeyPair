f69541db1bb782e32ecf5917f05d2c51be85da16
Joris Scharp <joris@headease.nl>
2023-06-16 09:58:05 +0200

    KTL-1: Exporting to the secrets folder, can easily mount the whole folder now

--- gen_keypair.sh
+++ gen_keypair.sh
@@ -1,8 +1,8 @@
-ssh-keygen -t rsa -m PKCS8 -b 2048 -f private.key
-openssl rsa -in private.key -pubout -outform PEM -out public.key
+ssh-keygen -t rsa -m PKCS8 -b 2048 -f ./secrets/private.pem
+openssl rsa -in private.pem -pubout -outform PEM -out ./secrets/public.pem
 
 printf "Private key:\n\n"
-cat private.key
+cat ./secrets/private.pem
 printf "\nPublic key:\n\n"
-cat public.key
+cat ./secrets/public.pem
 printf "\n"
